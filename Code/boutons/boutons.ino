035b52718bdc8c52c0749ebd3bcd52a228c871d3
Clarissecptr <clacla.carpentier@gmail.com>
2024-03-11 18:55:07 +0100

    boutons

---
+++ Code/boutons/boutons.ino
@@ -0,0 +1,100 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
+
+#define SCREEN_ADDRESS 0x3C
+
+enum State {
+  MAIN, SELECT_MENU
+};
+
+int instrument;
+
+State current_state = MAIN;
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+
+  display.setCursor(0, 0);  
+  display.write("Press any button");
+
+  display.setCursor(0, 7);
+  display.write("to continue");
+
+  display.display();
+
+  for(int i = 5;i <= 7; i++) {
+    pinMode(i, INPUT);
+  }
+  
+}
+
+void loop() {
+  if(current_state == MAIN) {
+    for(int i = 5;i <= 7; i++) {
+      if(digitalRead(i)) {
+        current_state = SELECT_MENU;
+        display.clearDisplay();
+        
+        display.setTextColor(SSD1306_WHITE);
+        display.setTextSize(1);
+
+        display.setCursor(0, 0);
+        display.write("1) Piano");
+
+        display.setCursor(0, 10);
+        display.write("2) Clarinette");
+        
+        display.setCursor(0, 20);
+        display.write("3) Trompette");
+        
+        display.display();
+        while(digitalRead(i)) {
+          Serial.print(digitalRead(i));
+        }
+      }
+    }
+  } else if(current_state == SELECT_MENU) {
+    for(int i = 5;i <= 7; i++) {
+      if(digitalRead(i)) {
+        instrument = i - 5;
+        display.clearDisplay();
+        
+        display.setTextColor(SSD1306_WHITE);
+        display.setTextSize(1);
+
+        display.setCursor(0, 0);
+        display.write("Playing : ");
+
+        display.setCursor(0, 10);
+        if(instrument == 0) {
+          display.write("Clarinette");
+        } else if(instrument == 1) {
+          display.write("Trompette");
+        } else if(instrument == 2) {
+          display.write("Piano");
+        }
+
+        display.display();
+        
+        current_state = MAIN;
+        while(digitalRead(i)) {
+          Serial.print(digitalRead(i));
+        }
+      }
+    }
+  }
+}
